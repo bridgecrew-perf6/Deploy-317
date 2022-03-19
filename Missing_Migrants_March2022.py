96e2667fc4f047ac170f14a6ef67682f5eed2c2e
liamcomerweaver <78572114+liamcomerweaver@users.noreply.github.com>
2022-03-19 16:16:17 -0300

    Update Missing_Migrants_March2022.py

--- Missing_Migrants_March2022.py
+++ Missing_Migrants_March2022.py
@@ -92,6 +92,8 @@ app = Dash(__name__, external_stylesheets = [dbc.themes.LUX],
                   meta_tags = [{'name' : 'viewport',
                                'content' : 'width-device-width, initial-scale=1.0'}]
                   )
+server = app.server
+
 
 app.layout = dbc.Container([
     
